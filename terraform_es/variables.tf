d9237b8f33a5538f476725869ed5718e04e142c7
Pravesh <Praveshgangwar04@gmail.com>
2024-09-30 19:59:40 +0530

    updated

--- terraform_es/variables.tf
+++ terraform_es/variables.tf
@@ -19,9 +19,11 @@ variable "peer_cidr_block" {
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
   type = string
+  default     = "10.0.1.0/24"  # Set a default value here
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR block for the private subnet"
   type        = string
+  default     = "10.0.2.0/24"  # Set a default value here
 }
